d5bb546a27d68b977d2b94890fa9b1ff72725da6
fsanche3 <fsanche3@oswego.edu>
2022-08-12 13:47:06 -0400

    Connected Like button to backend for Post controller && Modified Post.ts,components/post-feed, and the Post model to carry a array of users that liked a post. Lastly I positioned like button

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,9 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import Post from 'src/app/models/Post';
+import User from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { PostService } from 'src/app/services/post.service';
+//<style>
+//('.content').toggleClass("heart-active")
 
+   
+ // </style>
+  
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -16,12 +22,45 @@ export class PostComponent implements OnInit {
   })
 
   @Input('post') post: Post
+  users: User[];
   replyToPost: boolean = false
+  @Input() likeCount: number;
+  @Input() isActive: boolean;
 
-  constructor(private postService: PostService, private authService: AuthService) { }
+  constructor(private postService: PostService, private authService: AuthService) {
+   }
+
+   
 
   ngOnInit(): void {
   }
+
+  like(){  
+    this.postService.likePost(this.authService.currentUser.id,this.post.id)?.subscribe(
+      resp => {    
+        this.likeCount = resp.users.length;
+      }
+    )
+		this.isActive = !this.isActive;
+    let content = document.getElementById('content');
+    let heart = document.getElementById('heart');
+
+    if(!this.isActive)
+    {content?.style.setProperty('background-color', 'white')
+    this.postService.unlikePost(this.authService.currentUser.id, this.post.id)?.subscribe(
+      resp => {
+        this.likeCount = resp.users.length;
+      }
+    )} 
+    else{
+    content?.style.setProperty('background-color', '#f9b9c4');
+    heart?.style.setProperty('border-color', '#f9b9c4');
+    }
+    
+  }
+
+
+
 
   toggleReplyToPost = () => {
     this.replyToPost = !this.replyToPost
@@ -29,7 +68,7 @@ export class PostComponent implements OnInit {
 
   submitReply = (e: any) => {
     e.preventDefault()
-    let newComment = new Post(0, this.commentForm.value.text || "", "", this.authService.currentUser, [])
+    let newComment = new Post(0, this.commentForm.value.text || "", "", this.authService.currentUser, [],[])
     this.postService.upsertPost({...this.post, comments: [...this.post.comments, newComment]})
       .subscribe(
         (response) => {
@@ -38,4 +77,6 @@ export class PostComponent implements OnInit {
         }
       )
   }
+  
 }
+
