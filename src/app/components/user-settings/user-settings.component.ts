2fde062790e74e15cb8b206dec79eb1f20f7faca
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-12 15:03:16 -0400

    Added content to ngOnInit in userSettings.ts

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,25 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import User from '../../models/User';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-settings',
-  templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.css']
+    selector: 'app-user-settings',
+    templateUrl: './user-settings.component.html',
+    styleUrls: ['./user-settings.component.css']
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor() { }
+    profileImg: HTMLImageElement;
+    imgUrlText: HTMLInputElement;
+    usernameText: HTMLInputElement;
+    emailText: HTMLInputElement;
+    fNameText: HTMLInputElement;
+    lNameText: HTMLInputElement;
+    newPWText: HTMLInputElement;
+    confirmPWText: HTMLInputElement;
+    loggedIn: User;
 
-  ngOnInit(): void {
+    constructor(private router: Router) { }
 
-  }
+    ngOnInit(): void {
+        this.profileImg = <HTMLImageElement>document.getElementById("profileImg");
+        this.imgUrlText = <HTMLInputElement>document.getElementById("imgUrlText");
+        this.usernameText = <HTMLInputElement>document.getElementById("usernameText");
+        this.emailText = <HTMLInputElement>document.getElementById("emailText");
+        this.fNameText = <HTMLInputElement>document.getElementById("fNameText");
+        this.lNameText = <HTMLInputElement>document.getElementById("lNameText");
+        this.newPWText = <HTMLInputElement>document.getElementById("newPWText");
+        this.confirmPWText = <HTMLInputElement>document.getElementById("confirmPWText");
+        this.loggedIn = JSON.parse(<string>sessionStorage.getItem("user"));
 
-  // When user clicks the update button, the image URL changes to
-  // set their pfp with a new one.
-  uploadImage(){
-    document.getElementById("imageUrlText");
-  }
+        if (this.loggedIn == undefined) {
+            this.profileImg.src = "https://th.bing.com/th/id/OIP.61ajO7xnq1UZK2GVzHymEQAAAA?w=145&h=150&c=7&r=0&o=5&pid=1.7";
+            this.imgUrlText.value = "";
+            this.usernameText.value = "";
+            this.emailText.value = "";
+            this.fNameText.value = "";
+            this.lNameText.value = "";
+            this.router.navigate(["post-feed"])
+        } else {
+            this.profileImg.src = this.loggedIn.profileImg;
+            this.imgUrlText.value = this.loggedIn.profileImg;
+            this.usernameText.value = this.loggedIn.username
+            this.emailText.value = this.loggedIn.email;
+            this.fNameText.value = this.loggedIn.firstName;
+            this.lNameText.value = this.loggedIn.lastName;
+        }
+    }
 
-  updatePassword() {
-      
-  }
+    // When user clicks the update button, the image URL changes to
+    // set their pfp with a new one.
+    uploadImage() {
+        document.getElementById("imageUrlText");
+    }
+
+    updatePassword() {
+
+    }
 }
