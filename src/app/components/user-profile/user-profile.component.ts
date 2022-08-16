a5cc74f027baa79093ec0dc7d8c0ac758dbc3610
Hannah Bush <heb.rutgers@gmail.com>
2022-08-16 13:26:53 -0400

    merging user branch into personal; updated user profile component ts also

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import User from 'src/app/models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,21 +12,29 @@ export class UserProfileComponent implements OnInit {
   user: User = {} as User;
   profileImg: HTMLDivElement;
   usernameDisplay: HTMLParagraphElement;
+  usersPage: boolean = false;
+  usersPageId: number;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(<string>sessionStorage.getItem("user"));
-      console.log(this.user);
-      this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
-      this.usernameDisplay = <HTMLParagraphElement>document.getElementById("p-username");
-      if (this.user == undefined) {
-        this.profileImg.style.backgroundImage = "URL('https://th.bing.com/th/id/OIP.61ajO7xnq1UZK2GVzHymEQAAAA?w=145&h=150&c=7&r=0&o=5&pid=1.7')";
-        this.usernameDisplay.innerHTML = "Anonymous";
-      } else {
-        this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
-        this.usernameDisplay.innerHTML = this.user.username;
+    this.usersPageId = JSON.parse(<string>sessionStorage.getItem("usersPageId"));
+    console.log(this.user);
+    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
+    this.usernameDisplay = <HTMLParagraphElement>document.getElementById("p-username");
+
+    if (this.usersPageId == undefined) {
+      alert("We could not find this user! You're now being redirected.")
+      this.router.navigate(["post-feed"]);
+    } else {
+      this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
+      this.usernameDisplay.innerHTML = this.user.username;
+
+      if (this.usersPageId == this.user.id) {
+        this.usersPage = true;
       }
+    }
   }
 
 }
