7223c096adbbfd69112913cbbcb1c37c7ba43f8c
Hannah Bush <heb.rutgers@gmail.com>
2022-08-18 11:47:07 -0400

    got user profile component to a much more workable state; far from finished

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -13,6 +13,7 @@ import { __param } from 'tslib';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+
 export class UserProfileComponent implements OnInit, OnDestroy{
 
   user: User = {} as User;
@@ -28,7 +29,6 @@ export class UserProfileComponent implements OnInit, OnDestroy{
   })
 
   posts: Post[] = [];
-  createPost = false;
 
   constructor(private router: Router) { }
 
@@ -38,38 +38,27 @@ export class UserProfileComponent implements OnInit, OnDestroy{
       this.usersPageId = +params['id'];
     })
 
-    this.user = JSON.parse(<string>sessionStorage.getItem("user"));
-    //this.usersPageId = JSON.parse(<string>sessionStorage.getItem("usersPageId"));
-    console.log(this.user);
-    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
-    this.usernameDisplay = <HTMLParagraphElement>document.getElementById("p-username");
-
+    
     if (this.usersPageId == undefined) {
       alert("We could not find this user! You're now being redirected.")
-      //this.router.navigate(["post-feed"]);
+      this.router.navigate(["post-feed"]);
+    } else if (this.usersPageId == this.user.id) {
+      this.user = JSON.parse(<string>sessionStorage.getItem("user"));
+      this.usersPage = true;
     } else {
-      this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
-      this.usernameDisplay.innerHTML = this.user.username;
-
-      if (this.usersPageId == this.user.id) {
-        this.usersPage = true;
-      }
+      //this.user = fetch call to back end to get user details
+      //this.user = 0;
     }
+    console.log(this.user);
+
+    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
+    this.usernameDisplay = <HTMLParagraphElement>document.getElementById("prof-username");
+    this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
+    this.usernameDisplay.innerHTML = this.user.username;
   }
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
-
-  windowRef: Window;
-
-	openPopover(): void {
-        this.windowRef = <Window> window.open(
-          //add no opener safety feature
-			'router [user-settings]', //change to window settings, probably router [user-settings]
-			//'_blank', //probably opens new tab, possibly remove
-			'toolbar=0,menubar=0,width=500,height=500', // see https://developer.mozilla.org/en-US/docs/Web/API/Window/open#Window_features for a full list of features and what they do
-		)
-	}
 
 }
