a7afa336b27e0dd86e7cfb7d4e52d29be7a50a01
Hannah Bush <heb.rutgers@gmail.com>
2022-08-18 15:00:40 -0400

    got user profile ts working!

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import Post from 'src/app/models/Post';
 import User from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { PostService } from 'src/app/services/post.service';
+import { UserService } from 'src/app/services/user.service';
 import { __param } from 'tslib';
 
 @Component({
@@ -18,42 +18,45 @@ export class UserProfileComponent implements OnInit, OnDestroy{
 
   user: User = {} as User;
   profileImg: HTMLDivElement;
-  usernameDisplay: HTMLParagraphElement;
+  usernameDisplay: string;
   usersPage: boolean = false;
   usersPageId: number;
   sub: any;
-  route: ActivatedRoute;
   posts: Post[] = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UserService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-
+  ngOnInit() {
+    //this.usersPageId = 11;
     this.sub = this.route.params.subscribe(params => {
       this.usersPageId = +params['id'];
+      console.log(this.usersPageId);
     })
-
+    console.log(this.usersPageId);
+    this.user = JSON.parse(<string>sessionStorage.getItem("user"));
     
-    if (this.usersPageId == undefined) {
-      alert("We could not find this user! You're now being redirected.")
-      this.router.navigate(["post-feed"]);
-    } else if (this.usersPageId == this.user.id) {
-      this.user = JSON.parse(<string>sessionStorage.getItem("user"));
-      this.usersPage = true;
-    } else {
+    // if (this.usersPageId == undefined) {
+    //   alert("We could not find this user! You're now being redirected.")
+    //   this.router.navigate(["post-feed"]);
+    // } else if (this.usersPageId == this.user.id) {
+    //   this.usersPage = true;
+    // } else {
       //this.user = fetch call to back end to get user details
-      //this.user = 0;
-    }
-    console.log(this.user);
+      this.userService.getUserById(this.usersPageId)?.subscribe((resp: any ) => {
+        console.log(resp);
+        this.usernameDisplay = resp.username;
+        console.log(this.usernameDisplay);
+      });
+    //}
 
     this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
-    this.usernameDisplay = <HTMLParagraphElement>document.getElementById("prof-username");
     this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
-    this.usernameDisplay.innerHTML = this.user.username;
+    console.log(this.user.username);
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    console.log('should unsub from params if working');
+    //this.sub.unsubscribe();
   }
 
 }
