4c06b7c68682e2ed556f5b8a9e822ebcebe591bf
Hannah Bush <heb.rutgers@gmail.com>
2022-08-18 17:15:07 -0400

    updated profile, need to add getpostsbyuserid in BE

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -19,39 +19,49 @@ export class UserProfileComponent implements OnInit, OnDestroy{
   user: User = {} as User;
   profileImg: HTMLDivElement;
   usernameDisplay: string;
+  nameDisplay: string;
   usersPage: boolean = false;
   usersPageId: number;
   sub: any;
   posts: Post[] = [];
 
-  constructor(private router: Router, private userService: UserService, private route: ActivatedRoute) { }
+  constructor(private router: Router, private userService: UserService, private postService: PostService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    //this.usersPageId = 11;
     this.sub = this.route.params.subscribe(params => {
       this.usersPageId = +params['id'];
       console.log(this.usersPageId);
     })
     console.log(this.usersPageId);
     this.user = JSON.parse(<string>sessionStorage.getItem("user"));
+    console.log(this.user.id);
+    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
     
-    // if (this.usersPageId == undefined) {
-    //   alert("We could not find this user! You're now being redirected.")
-    //   this.router.navigate(["post-feed"]);
-    // } else if (this.usersPageId == this.user.id) {
-    //   this.usersPage = true;
-    // } else {
+    if (this.usersPageId == undefined) {
+      alert("We could not find this user! You're now being redirected.")
+      this.router.navigate(["post-feed"]);
+    } else if (this.usersPageId == this.user.id) {
+      this.usersPage = true;
+      this.usernameDisplay = this.user.username;
+      this.nameDisplay = `${this.user.firstName} ${this.user.lastName}`;
+      this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
+    } else {
       //this.user = fetch call to back end to get user details
       this.userService.getUserById(this.usersPageId)?.subscribe((resp: any ) => {
         console.log(resp);
         this.usernameDisplay = resp.username;
         console.log(this.usernameDisplay);
+        this.nameDisplay = `${resp.firstName} ${resp.lastName}`;
+        console.log(this.nameDisplay);
+        this.profileImg.style.backgroundImage = "URL('" + resp.profileImg + "')";
       });
-    //}
-
-    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
-    this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
-    console.log(this.user.username);
+    }
+    
+    this.postService.getAllPostsByUserID(this.usersPageId).subscribe(
+      (response : any) => {
+        this.posts = response
+      }
+    )
   }
 
   ngOnDestroy(): void {
