fe60b85512b34700f7196d4aa6bcd7f0192798aa
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-08-19 12:13:48 -0400

    likes fix

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,9 +10,10 @@ import Post from '../models/Post';
 export class PostService {
 
   postUrl = `${environment.baseUrl}/post`
-  postGetLikesUrl = `${environment.baseUrl}/post`
   postLikeUrl = `${environment.baseUrl}/post/like`
   postUnlikeUrl = `${environment.baseUrl}/post/unlike`
+  postGetLikesUrl = `${environment.baseUrl}/post`
+
 
   constructor(private http: HttpClient) { }
 
@@ -25,19 +26,21 @@ export class PostService {
   }
 
   likePost(userId: number, postId: number): Observable<Post>{
+ 
     return this.http.put<Post>(`${this.postLikeUrl}`, {postId , userId} , {headers: environment.headers, withCredentials: environment.withCredentials})
   }
 
   unlikePost(userId: number, postId: number): Observable<Post>{
-    return this.http.put<Post>(`${this.postUnlikeUrl}`, {postId, userId}, {headers: environment.headers, withCredentials: environment.withCredentials})
+
+  return this.http.put<Post>(`${this.postUnlikeUrl}`, {postId, userId}, {headers: environment.headers, withCredentials: environment.withCredentials})
+
   }
 
-  getAllPostsByUserID(): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.postUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials})
+  getPost( post: Post): Observable<Post>{
+ 
+    return this.http.get<Post>(`${this.postGetLikesUrl}/${post.id}`, {headers: environment.headers, withCredentials: environment.withCredentials})
   }
 
-  getPost(post: Post): Observable<Post>{
-    return this.http.get<Post>(`${this.postGetLikesUrl}/${post}`, {headers: environment.headers, withCredentials: environment.withCredentials})
-  }
+  
 
 }
