6ade2f4c2c770542bd5ba3b88164bd381517c591
New Pagodi <NewPagodi@users.noreply.github.com>
2022-08-20 17:16:39 -0500

    Address sonarcloud issues in PR104

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-//import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social-media-angular';
-  //nav = NavbarComponent;
 
 
 }
