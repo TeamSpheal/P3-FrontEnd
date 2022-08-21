bf13b899d0b332e526b264b396d4f54f8a572514
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-21 00:47:21 -0400

    Wrote tests for login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,25 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { from, of } from 'rxjs'
+import User from '../../models/User';
+import { HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let loginComp: LoginComponent;
+    let authServ: AuthService;
+    let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [LoginComponent]
+        })
+        loginComp = TestBed.inject(LoginComponent);
+        authServ = TestBed.inject(AuthService);
+        router = TestBed.inject(Router);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(loginComp).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('onSubmit: should set "user" and "JWT" in local storage', () => {
+        /*Local Variables*/
+        const mockUser: User = new User(1, 'testuser@gmail.com', 'Test', 'User',
+            'TestUser1', 'assets/images/favicon.png', [], []);
+        const mockHeaders = new HttpHeaders({
+            "Auth": "JWTToken"
+        });
+        const mockResp: HttpResponse<any> = new HttpResponse({
+            body: JSON.stringify(mockUser),
+            headers: mockHeaders
+        });
+
+        /*Mocks*/
+        spyOn(authServ, 'login').and.callFake(() => {
+            return from(of(mockResp));
+        });
+
+        /*Function*/
+        loginComp.onSubmit(event);
+
+        /*Test*/
+        expect(localStorage.getItem("user")).toBeTruthy();
+        expect(localStorage.getItem("JWT")).toBeTruthy();
+    });
+
+    it('register: should redirect to the register component', () => {
+        /*Mocks*/
+        spyOn(router, 'navigate');
+
+        /*Function*/
+        loginComp.register();
+
+        /*Test*/
+        expect(router.navigate).toHaveBeenCalled();
+    });
 });
