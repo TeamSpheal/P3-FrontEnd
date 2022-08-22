6ae4abe3a334805c1089fc0b4bddd9c449da73fc
Hannah Bush <heb.rutgers@gmail.com>
2022-08-22 16:00:06 -0400

    following btn updates

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,29 +11,31 @@ import User from '../models/User';
 })
 export class UserService {
   userUrl: string = `${environment.baseUrl}/user`
-  userUpadatedUrl: string;
+  userUpdatedUrl: string;
   isFollowResp: Observable<any>;
   isFollow: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   addFollower(followedId: number, followerId:number): Observable<any> {
-    this.userUpadatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
-    return (this.http.post(`${this.userUpadatedUrl}`, null, {headers: environment.headers, withCredentials: environment.withCredentials}));
+    this.userUpdatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
+    return (this.http.post(`${this.userUpdatedUrl}`, null, {headers: environment.headers, withCredentials: environment.withCredentials}));
   }
 
   removeFollower(followedId: number, followerId: number): Observable<any> {
-    this.userUpadatedUrl = `${this.userUrl}/${followedId}/unfollow/${followerId}`; 
-    return this.http.delete(this.userUpadatedUrl, { headers: environment.headers, withCredentials: environment.withCredentials });
+    this.userUpdatedUrl = `${this.userUrl}/${followedId}/unfollow/${followerId}`; 
+    return this.http.delete(this.userUpdatedUrl, { headers: environment.headers, withCredentials: environment.withCredentials });
   }
 
   isFollowing(followedId: number, followerId: number): boolean {
-    this.userUpadatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
-    this.isFollowResp = this.http.get(`${this.userUpadatedUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials});
-    console.log(this.isFollowResp.pipe(isEmpty()));
-    if(this.isFollowResp.pipe(isEmpty())) {
-      this.isFollow = true;
-    }
+    this.userUpdatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
+    this.isFollowResp = this.http.get(`${this.userUpdatedUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials});
+    console.log(this.isFollowResp.forEach(element => {
+      console.log(element);
+    }));
+    // if(this.isFollowResp.pipe(isEmpty())) {
+    //   this.isFollow = true;
+    // }
     return(this.isFollow);
   }
 
