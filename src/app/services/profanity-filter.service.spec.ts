443346b4dab34a913a17a6ffa15013dca0ed1d50
Mike Pepin <pepin_michael@wheatoncollege.edu>
2022-08-23 12:24:46 -0400

    added profanity filter

---
+++ src/app/services/profanity-filter.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ProfanityFilterService } from './profanity-filter.service';
+
+describe('ProfanityFilterService', () => {
+  let service: ProfanityFilterService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ProfanityFilterService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
