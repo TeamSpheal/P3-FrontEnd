d3a70c4ef360f7d302d80b5c756d5692f2953ae4
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-23 00:29:48 -0400

    Worked on follow/user-profile feature

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -7,73 +7,86 @@ import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.css']
 })
 
-export class UserProfileComponent implements OnInit, OnDestroy{
+export class UserProfileComponent implements OnInit, OnDestroy {
 
-  user: User = {} as User;
-  profileImg: HTMLDivElement;
-  usernameDisplay: string;
-  nameDisplay: string;
-  followers: UserMiniDTO[];
-  following: UserMiniDTO[];
-  usersPage = false;
-  usersPageId: number;
-  sub: any;
-  posts: Post[] = [];
-  postCount = 0;
-  constructor(private router: Router, private userService: UserService, private postService: PostService, private route: ActivatedRoute) { }
+    user: User = {} as User;
+    userMiniDTO: UserMiniDTO;
+    profileImg: HTMLDivElement;
+    usernameDisplay: string;
+    nameDisplay: string;
+    followers: UserMiniDTO[];
+    following: UserMiniDTO[];
+    followerCount: number;
+    followingCount: number;
+    usersPage = false;
+    usersPageId: number;
+    sub: any;
+    posts: Post[] = [];
+    postCount = 0;
+    constructor(private router: Router, private userService: UserService, private postService: PostService, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    //gets id from param
-    this.sub = this.route.params.subscribe(params => {
-      this.usersPageId = +params['id'];
-      console.log(this.usersPageId);
-    })
-    //gets id from logged in user
-    this.user = JSON.parse(<string>localStorage.getItem("user"));
-    console.log(this.user.id);
-    this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
-    
-    if (this.usersPageId == undefined) {
-      alert("We could not find this user! You're now being redirected.")
-      this.router.navigate(["post-feed"]);
-    } else if (this.usersPageId == this.user.id) {
-      this.usersPage = true;
-      this.usernameDisplay = this.user.username;
-      this.nameDisplay = `${this.user.firstName} ${this.user.lastName}`;
-      this.followers = this.user.followers;
-      this.following = this.user.following;
-      this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
-    } else {
-      //this.user = fetch call to back end to get user details
-      this.userService.getUserById(this.usersPageId)?.subscribe((resp: any ) => {
-        this.usernameDisplay = resp.username;
-        this.nameDisplay = `${resp.firstName} ${resp.lastName}`;
-        this.followers = resp.followers;
-        console.log('user service was called');
-        console.log(this.followers.length + 'followers length');
-        this.following = resp.following;
-        //console.log(this.followings.length + 'followings length');
-        this.profileImg.style.backgroundImage = "URL('" + resp.profileImg + "')";
-      });
+    ngOnInit() {
+        //gets id from param
+        this.sub = this.route.params.subscribe(params => {
+            this.usersPageId = +params['id'];
+            localStorage.setItem("usersPageId", this.usersPageId.toString())
+        })
+        //gets id from logged in user
+        this.user = JSON.parse(<string>localStorage.getItem("user"));
+        this.profileImg = <HTMLDivElement>document.getElementById("user-circle");
+
+        if (this.usersPageId == undefined) {
+            alert("We could not find this user! You're now being redirected.")
+            this.router.navigate(["post-feed"]);
+        } else if (this.usersPageId == this.user.id) {
+            this.usersPage = true;
+            this.usernameDisplay = this.user.username;
+            this.nameDisplay = `${this.user.firstName} ${this.user.lastName}`;
+            this.followers = this.user.followers;
+            this.following = this.user.following;
+            this.profileImg.style.backgroundImage = "URL('" + this.user.profileImg + "')";
+        } else {
+            //this.user = fetch call to back end to get user details
+            this.userService.getUserById(this.usersPageId)?.subscribe((resp: any) => {
+                this.usernameDisplay = resp.username;
+                this.nameDisplay = `${resp.firstName} ${resp.lastName}`;
+                this.followers = resp.followers;
+                this.following = resp.following;
+                //console.log(this.followings.length + 'followings length');
+                this.profileImg.style.backgroundImage = "URL('" + resp.profileImg + "')";
+                this.userMiniDTO = new UserMiniDTO(resp.id, resp.username, resp.profileImg);
+                localStorage.setItem("viewingUser", JSON.stringify(this.userMiniDTO));
+            });
+        }
+
+        if (this.followers) {
+            this.followerCount = this.followers.length;
+        } else {
+            this.followerCount = 0;
+        }
+        if (this.following) {
+            this.followingCount = this.following.length;
+        } else {
+            this.followingCount = 0;
+        }
+
+        this.postService.getAllPostsByUserID(this.usersPageId).subscribe(
+            (response: any) => {
+                console.log(response)
+                this.posts = response
+                this.postCount = this.posts.length;
+            }
+        )
     }
-    
-    this.postService.getAllPostsByUserID(this.usersPageId).subscribe(
-      (response : any) => {
-        console.log(response)
-        this.posts = response
-        this.postCount = this.posts.length;
-      }
-    )
-  }
 
-  ngOnDestroy(): void {
-    //console.log('should unsub from params if working');
-    this.sub.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        //console.log('should unsub from params if working');
+        this.sub.unsubscribe();
+    }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { of } from 'rxjs';
-import { isEmpty } from 'rxjs/operators';  
 import { environment } from 'src/environments/environment';
 import User from '../models/User';
 import UserMiniDTO from '../models/UserMiniDTO';
@@ -11,61 +9,40 @@ import UserMiniDTO from '../models/UserMiniDTO';
     providedIn: 'root'
 })
 export class UserService {
-  userUrl: string = `${environment.baseUrl}/user`
-  userUpdatedUrl: string;
-  isFollowResp: UserMiniDTO[];
-  isFollow: boolean = false;
-  user: User = JSON.parse(<string>localStorage.getItem("user"));
+    userUrl: string = `${environment.baseUrl}/user`
+    userUpdatedUrl: string;
+    isFollowResp: UserMiniDTO[];
+    isFollow: boolean = false;
+    user: User = JSON.parse(<string>localStorage.getItem("user"));
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  addFollower(followedId: number, followerId:number): void {
-    let response: any | undefined;
-    this.userUpdatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
-    this.http.post(`${this.userUpdatedUrl}`, null, 
-    {headers: environment.headers, withCredentials: environment.withCredentials})
-    .subscribe((data: any) => {
-      //Parse data
-      if (data != undefined) {
-          response = JSON.parse(JSON.stringify(data));
-      }
+    addFollower(followedId: number, followerId: number): Observable<any> {
+        this.userUpdatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
+        return this.http.post(`${this.userUpdatedUrl}`, null,
+            { headers: environment.headers, withCredentials: environment.withCredentials })
+    }
 
-      //Process data
-      if (response != undefined) { //Data is defined. The return from the response should contain a user object
-          alert(
-              "Your follow this user successfully!"
-          );
-      } else { //Data is undefined, meaning the request failed
-          alert(
-              "The server failed to follow this user"
-          );
-      }
-    });
-  }
+    removeFollower(followedId: number, followerId: number): Observable<any> {
+        this.userUpdatedUrl = `${this.userUrl}/${followedId}/unfollow/${followerId}`;
+        return this.http.delete(this.userUpdatedUrl, { headers: environment.headers, withCredentials: environment.withCredentials });
+    }
 
-  removeFollower(followedId: number, followerId: number): Observable<any> {
-    this.userUpdatedUrl = `${this.userUrl}/${followedId}/unfollow/${followerId}`; 
-    return this.http.delete(this.userUpdatedUrl, { headers: environment.headers, withCredentials: environment.withCredentials });
-  }
+    isFollowing(viewingUser: UserMiniDTO): boolean {
+        let loggedIn = JSON.parse(<string>localStorage.getItem("user"));
+        let followingList = loggedIn.following as UserMiniDTO[];
+        for (let i = 0; i < followingList.length && !this.isFollow; i++) {
+            if (followingList[i].id == viewingUser.id) {
+                this.isFollow = true;
+            }
+        }
 
-  isFollowing(followedId: number, followerId: number): boolean {
-    // this.userUpdatedUrl = `${this.userUrl}/${followedId}/follower/${followerId}`;
-    // this.isFollowResp = this.http.get(`${this.userUpdatedUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials});
-    this.isFollowResp = this.user.following;
-    // for(let i = 0; i < this.isFollowResp.length; i++) {
-    //   if(isF)
-    // }
-    console.log(this.user);
-    console.log(this.user.followers);
-    console.log(this.user.following);
-    console.log(this.isFollowResp);
+        return (this.isFollow);
+    }
 
-    return(this.isFollow);
-  }
-  
-  getUserById(id: number): Observable<any> {
-    return this.http.get<User>(`${this.userUrl}/${id}`, { headers: environment.headers, withCredentials: environment.withCredentials });
-  }
+    getUserById(id: number): Observable<any> {
+        return this.http.get<User>(`${this.userUrl}/${id}`, { headers: environment.headers, withCredentials: environment.withCredentials });
+    }
 }
 
 
