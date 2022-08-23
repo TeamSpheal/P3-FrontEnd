81aaf1eb6736b20bd06762e997840e6d20bfb8fb
Colby Tang <cobtang@gmail.com>
2022-08-23 18:26:48 -0500

    Addressed the ESLint

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,61 +1,63 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import Post from 'src/app/models/Post';
-import { AuthService } from 'src/app/services/auth.service';
-import { PostService } from 'src/app/services/post.service';
-import { ProfanityFilterService } from 'src/app/services/profanity-filter.service';
-import UserMiniDTO from 'src/app/models/UserMiniDTO';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import Post from "src/app/models/Post";
+import { AuthService } from "src/app/services/auth.service";
+import { PostService } from "src/app/services/post.service";
+import { ProfanityFilterService } from "src/app/services/profanity-filter.service";
+import UserMiniDTO from "src/app/models/UserMiniDTO";
 
 @Component({
-  selector: 'app-comment',
-  templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  selector: "app-comment",
+  templateUrl: "./comment.component.html",
+  styleUrls: ["./comment.component.css"],
 })
 export class CommentComponent implements OnInit {
-  replyState: boolean = false;
+  replyState = false;
   commentForm = new FormGroup({
-    text: new FormControl(''),
-  })
+    text: new FormControl(""),
+  });
 
-  @Input('comment') inputComment: Post;
-  replyToComment = false
+  @Input("comment") inputComment: Post;
+  replyToComment = false;
   likeState = "Like";
   @Input() likeCount: number;
   @Input() isActive: boolean;
   @Input() isNotActive = false;
 
-  constructor(private postService: PostService, private authService: AuthService, private profanityService: ProfanityFilterService) { }
+  constructor(
+    private postService: PostService,
+    private authService: AuthService,
+    private profanityService: ProfanityFilterService
+  ) {}
 
   ngOnInit(): void {
-    if(localStorage.getItem("user")){
-      this.authService.currentUser=JSON.parse(<string>localStorage.getItem("user"));
+    if (localStorage.getItem("user")) {
+      this.authService.currentUser = JSON.parse(
+        <string>localStorage.getItem("user")
+      );
     }
-    //this.isLiked();
 
     this.likeCount = this.inputComment.users.length;
-    // Init to be filled in later
   }
-  isLiked(){
+  isLiked() {
+    const curUser = new UserMiniDTO(
+      this.authService.currentUser.id,
+      this.authService.currentUser.username,
+      this.authService.currentUser.profileImg
+    );
 
-    const curUser = new UserMiniDTO(this.authService.currentUser.id,
-                                    this.authService.currentUser.username,
-                                    this.authService.currentUser.profileImg);
+    const likedIds = this.inputComment.users.map((x) => x.id);
 
-    const likedIds = this.inputComment.users.map(x =>x.id);
+    const button = document.getElementById("rlikeBtn-" + this.inputComment.id);
 
-    const button = document.getElementById('rlikeBtn-' + this.inputComment.id);
-    
-    if ( likedIds.includes(curUser.id) ){
+    if (likedIds.includes(curUser.id)) {
       this.isActive = true;
-      this.likeState = "Liked"
-      button?.style.setProperty('color','#ef773b');
-
-      //console.log(button?.style.getPropertyValue('background'));
-    }
-    else {
+      this.likeState = "Liked";
+      button?.style.setProperty("color", "#ef773b");
+    } else {
       this.isActive = false;
-      this.likeState = "Like"
-      button?.style.setProperty('color','unset');
+      this.likeState = "Like";
+      button?.style.setProperty("color", "unset");
     }
   }
 
@@ -63,63 +65,72 @@ export class CommentComponent implements OnInit {
     this.isLiked();
   }
 
-  like(){  
-    const button = document.getElementById('rlikeBtn-' + this.inputComment.id);
+  like() {
+    const button = document.getElementById("rlikeBtn-" + this.inputComment.id);
 
-    if(!this.isActive) {
-      this.postService.likePost(this.authService.currentUser.id,this.inputComment.id)?.subscribe(
-        (      resp: { users: string | any[]; }) => {
+    if (!this.isActive) {
+      this.postService
+        .likePost(this.authService.currentUser.id, this.inputComment.id)
+        ?.subscribe((resp: { users: string | any[] }) => {
           this.likeCount = resp.users.length;
           this.isActive = true;
-          this.likeState = "Liked"
-          button?.style.setProperty('color','#ef773b');
-        }
-      )
+          this.likeState = "Liked";
+          button?.style.setProperty("color", "#ef773b");
+        });
     } else {
-      this.postService.unlikePost(this.authService.currentUser.id, this.inputComment.id)?.subscribe(
-        (      resp: { users: string | any[]; }) => {
+      this.postService
+        .unlikePost(this.authService.currentUser.id, this.inputComment.id)
+        ?.subscribe((resp: { users: string | any[] }) => {
           this.likeCount = resp.users.length;
           this.isActive = false;
-          this.likeState = "Like"
-          button?.style.setProperty('color','unset');
-        }
-      )
+          this.likeState = "Like";
+          button?.style.setProperty("color", "unset");
+        });
     }
   }
 
-  
   toggleReplyToComment = () => {
-    const reply = document.getElementById('replyBtn-' + this.inputComment.id);
-    this.replyToComment = !this.replyToComment
-    this.replyState = !this.replyState
+    const reply = document.getElementById("replyBtn-" + this.inputComment.id);
+    this.replyToComment = !this.replyToComment;
+    this.replyState = !this.replyState;
 
-    if(this.replyState){
-      reply?.style.setProperty('color','#ef773b');
-
-    }else{
-      reply?.style.setProperty('color','unset');
+    if (this.replyState) {
+      reply?.style.setProperty("color", "#ef773b");
+    } else {
+      reply?.style.setProperty("color", "unset");
     }
-  }
+  };
 
   submitReply = (e: any) => {
-    e.preventDefault()
-    if(this.commentForm.value.text) {
-      //Mike added this line here for profanity filter
-      this.commentForm.value.text = this.profanityService.cleanText(this.commentForm.value.text);
-      const newComment = new Post(0, this.commentForm.value.text || "", "", this.authService.currentUser, [], [], new Date());
-      this.postService.upsertPost({...this.inputComment, comments: [...this.inputComment.comments, newComment]})
-        .subscribe(
-          (response : any) => {
-            this.inputComment = response
-            this.toggleReplyToComment()
-          }
-        )
+    e.preventDefault();
+    if (this.commentForm.value.text) {
+      // Mike added this line here for profanity filter
+      this.commentForm.value.text = this.profanityService.cleanText(
+        this.commentForm.value.text
+      );
+
+      const newComment = new Post(
+        0,
+        this.commentForm.value.text || "",
+        "",
+        this.authService.currentUser,
+        [],
+        [],
+        new Date()
+      );
+      
+      this.postService
+        .upsertPost({
+          ...this.inputComment,
+          comments: [...this.inputComment.comments, newComment],
+        })
+        .subscribe((response: any) => {
+          this.inputComment = response;
+          this.toggleReplyToComment();
+        });
     } else {
-      console.log("error")
+      console.log("error");
       this.replyToComment = false;
     }
-  }
-
-
-
+  };
 }
